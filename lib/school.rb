1445cac101584c3b4b4231740eadfab6d97ec7ac
Austin <Austin_MacBook@Austins-MacBook-Pro.local>
2018-11-26 13:59:12 -0600

    first commit

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,38 @@
-# code here!
+class School
+  
+  attr_accessor :name, :roster
+  
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+  
+  def add_student(name, grade)
+    if @roster.has_key?(grade)
+      @roster[grade] << name
+    else
+      @roster[grade] = [name]
+    end
+  end
+  
+  def grade(grade)
+    @roster[grade]
+  end
+  
+  def sort
+    roster.each do |k, v|
+      v.sort!
+    end
+    roster
+  end
+  
+end
+
+# school = School.new('Flatiron')
+# school.add_student('Austin Puckett', 11)
+# school.add_student('Jakob Bellamy', 11)
+# school.add_student('Francisco Banda', 9)
+# school.add_student('Claire Nguyen', 10)
+# school.sort
+# school.roster
+# school.grade(11)
